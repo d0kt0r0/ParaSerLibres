83d9b04a78eae102cc9e12adb55ab4e3ca90901e
d0kt0r0 <ogbornd@mcmaster.ca>
2016-06-29 11:32:08 -0400

    various fixes to sembrar.js

--- sembrar.js
+++ sembrar.js
@@ -2,10 +2,11 @@ var WebSocket = require('ws');
 var osc = require('osc');
 
 var url = process.argv[2];
+var password = process.argv[3];
 var ws = new WebSocket(url);
 console.log("connecting to " + url + "...");
 ws.on('open', function() { console.log("websocket connection opened"); });
-ws.on('message', function() { console.log("message received on websocket"); });
+ws.on('message', function() {}); // not getting anything back from apert right now
 
 var udp = new osc.UDPPort( { localAddress: "127.0.0.1", localPort: 8000 });
 if(udp!=null) udp.open();
@@ -21,8 +22,8 @@ FragmentCollector.prototype.cojer = function (n,count,text) {
     this.text = null;
     return;
   }
-  if(n==0) this.text = t;
-  else this.text = this.text + t;
+  if(n==0) this.text = text;
+  else this.text = this.text + text;
   if(n<(count-1)) {
     // still more fragments to come
     this.expected = n+1;
@@ -39,7 +40,7 @@ var editFragments = new FragmentCollector();
 var evalFragments = new FragmentCollector();
 
 function request(x) {
-  x.password = process.argv[1];
+  x.password = password;
   try { ws.send(JSON.stringify(x)); }
   catch(e) { console.log("ERROR: exception in websocket send for request"); }
 }
@@ -48,7 +49,10 @@ udp.on('message', function(m) {
   if(m.address == "/edit") {
     if(m.args.length != 3) { console.log("ERROR: /edit must have 3 arguments"); return; }
     var ready = editFragments.cojer(m.args[0],m.args[1],m.args[2]);
-    if(ready) request({ request: 'write', key: 'paraSembrarLibres', value: editFragments.text });
+    if(ready) {
+      request({ request: 'write', key: 'paraSerLibres', value: editFragments.text });
+      request({ request: 'all', name: 'edit', args: [] });
+    }
   }
   else if (m.address == "/eval") {
     if(m.args.length != 3) { console.log("ERROR: /eval must have 3 arguments"); return; }
