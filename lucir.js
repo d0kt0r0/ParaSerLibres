a50359a1002e9d78a47f7af76a4b957b5f3e1219
d0kt0r0 <ogbornd@mcmaster.ca>
2016-07-07 11:25:51 -0400

    mulit-column style in lucir.js and auto-read when websocket is first opened

--- lucir.js
+++ lucir.js
@@ -1,18 +1,34 @@
 // ...para ser libres...
 
 $(document).ready(function() {
-  $(document.body).append('<div><pre id="pslText">...esperando...</pre></div>');
+  $(document.body).append('<div id="code">...esperando...</pre></div>');
+  $(document.body).css({
+    background-color: "black"
+  });
+  $('#code').css({
+    font-size: "0.95vh",
+    color: "green",
+//    -webkit-column-count: "4",
+//    -webkit-column-gap: "5px",
+//    -moz-column-count: "4",
+//    -moz-column-gap: "5px",
+    column-count: "4",
+    column-gap: "5px"
 });
 
+function apertWebSocketOpened() {
+  edit();
+}
+
 function edit() {
-  console.log("edit");
   apertGlobalRead('pslText');
 }
 
 function apertReceivedRead(key,value) {
   console.log("apertReceivedRead");
   if(key == 'pslText') {
-    $('#pslText').text(value);
+    value = (value + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ '<br/>' +'$2');
+    $('#code.inner').replaceWith(value);
   }
 }
 
